7b147fdd23589f6ab957ab437947f9cb361de981
Maia Teegarden <padmaia.thedev@gmail.com>
2018-10-24 10:46:56 -0700

    ignore node_modules in babel-register

--- packages/babel-register/index.js
+++ packages/babel-register/index.js
@@ -1,7 +1,9 @@
 if (!process.env.BABEL_ENV) process.env.BABEL_ENV = 'modern';
 
 require('@babel/register')({
-  ignore: [],
+  ignore: [
+    filepath => filepath.includes('/node_modules/')
+  ],
   presets: [
     ['@babel/env', {
       targets: {
